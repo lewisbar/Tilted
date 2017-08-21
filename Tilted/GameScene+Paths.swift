f311cef49e4107708faecde52e4cf9a658f66421
Lennart Wisbar <lewisbar.git@gmail.com>
2017-08-21 15:03:33 +0200

    Started restructuring
    Everything worked fine, except that touches moving from one control to another one couldn't be properly handled, as every view element was responsible for its own touches. To change that, the GameScene itself should be responsible for any touches. Also, it is planned to remove some classes like CornerButton to try and simplify the structure of the progam as far as possible.
    Right now, the game is not working properly, but it's on its way to work better than before. Hopefully.

---
+++ Tilted/GameScene+Paths.swift
@@ -0,0 +1,50 @@
+//
+//  Paths.swift
+//  Tilted
+//
+//  Created by Lennart Wisbar on 20.08.17.
+//  Copyright © 2017 Lennart Wisbar. All rights reserved.
+//
+
+import SpriteKit
+
+extension GameScene {
+    var fireButtonPath: CGPath {
+        let path = CGMutablePath()
+        let c = size.bottomLeft
+        let points = [
+            c,
+            CGPoint(x: c.x + size.width, y: c.y),
+            CGPoint(x: c.x, y: c.y + size.height)
+        ]
+        path.addLines(between: points)
+        return path
+    }
+    
+    var pauseButtonPath: CGPath {
+        let path = CGMutablePath()
+        let c = size.bottomLeft
+        let points = [
+            c,
+            CGPoint(x: c.x, y: c.y - size.height),
+            CGPoint(x: c.x - size.width, y: c.y)
+        ]
+        path.addLines(between: points)
+        return path
+    }
+    
+    var backgroundPath: CGPath {
+        let path = CGMutablePath()
+        let points = [
+            CGPoint(x: buttonSize.width, y: 0),
+            CGPoint(x: size.width, y: 0),
+            CGPoint(x: size.width, y: size.height - buttonSize.height),
+            CGPoint(x: size.width - buttonSize.width, y: size.height),
+            CGPoint(x: 0, y: size.height),
+            CGPoint(x: 0, y: buttonSize.height),
+            CGPoint(x: buttonSize.width, y: 0)
+        ]
+        path.addLines(between: points)
+        return path
+    }
+}
---
+++ Tilted/GameScene+TouchHandling.swift
@@ -0,0 +1,80 @@
+//
+//  GameScene+TouchHandling.swift
+//  Tilted
+//
+//  Created by Lennart Wisbar on 15.08.17.
+//  Copyright © 2017 Lennart Wisbar. All rights reserved.
+//
+
+import SpriteKit
+
+extension GameScene {
+    
+    override func update(_ currentTime: TimeInterval) {
+        spaceship.updateMovement()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if fireButtonPath.contains(location) {
+                let shootingVector = CGVector(dx: -self.size.width, dy: self.size.height)
+                spaceship.startShooting(with: shootingVector, zPosition: ZPositions.shot)
+            } else if pauseButtonPath.contains(location) {
+                isPaused = true
+            }
+        }
+        moveSpaceshipToClosestBackgroundTouch(event: event)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // TODO: If the last touch leaves the fire button, stop shooting (using "previousLocation"
+        for touch in touches {
+            let location = touch.location(in: self)
+            if fireButtonPath.contains(location) {
+                let shootingVector = CGVector(dx: -self.size.width, dy: self.size.height)
+                spaceship.startShooting(with: shootingVector, zPosition: ZPositions.shot)
+            } else if pauseButtonPath.contains(location) {
+                isPaused = true
+            }
+        }
+        moveSpaceshipToClosestBackgroundTouch(event: event)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if fireButtonPath.contains(location) {
+                // Stop shooting if there are no touches left on the fire button
+                guard let allTouches = event?.allTouches else {
+                    spaceship.stopShooting()
+                    return
+                }
+                let remainingFireButtonTouches = allTouches.filter { $0.phase != .ended && fireButtonPath.contains($0.location(in: self)) }
+                if remainingFireButtonTouches.isEmpty {
+                    spaceship.stopShooting()
+                }
+            }
+        }
+        moveSpaceshipToClosestBackgroundTouch(event: event)
+    }
+        
+    
+    // Helpers
+    private func moveSpaceshipToClosestBackgroundTouch(event: UIEvent?) {
+        guard let allTouches = event?.allTouches else { return }
+        let remainingBackgroundTouches = allTouches.filter { $0.phase != .ended && backgroundPath.contains($0.location(in: self)) }
+        let positions = remainingBackgroundTouches.map { $0.location(in: self)}
+        spaceship.flyingTarget = closestPositionToSpaceship(of: positions)
+    }
+    
+    private func closestPositionToSpaceship(of positions: [CGPoint]) -> CGPoint? {
+        let sortedPositions = positions.sorted { distanceFromSpaceship(to: $0) < distanceFromSpaceship(to: $1) }
+        return sortedPositions.first
+    }
+    
+    private func distanceFromSpaceship(to position: CGPoint) -> CGFloat {
+        return hypot(spaceship.handle.x - position.x,
+                     spaceship.handle.y - position.y)
+    }
+}
